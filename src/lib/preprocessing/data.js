f9111c5c75dcb45ad02f7ec5c8f80ac4e6792982
Jason Shin <visualbbasic@gmail.com>
2018-04-29 12:54:35 +1000

    onehot encoder

--- src/lib/preprocessing/data.js
+++ src/lib/preprocessing/data.js
@@ -3,12 +3,13 @@ exports.__esModule = true;
 var _ = require("lodash");
 var OneHotEncoder = /** @class */ (function () {
     function OneHotEncoder() {
+        this.booleanOneHot = function (list) { return _.map(list, function (value) { return (value ? 1 : 0); }); };
     }
     /**
      * encode
      *
      * @param data - list of records to encode
-     * @param opts - dataKeys: independent variables, labelKeys: dependent variables
+     * @param opts - dataKeys: independent variables, labelKeys: dependent variables; mandatory
      * @return {{data: Array, decoders: Array}} - see readme for full explanation
      */
     OneHotEncoder.prototype.encode = function (data, opts) {
@@ -18,7 +19,6 @@ var OneHotEncoder = /** @class */ (function () {
         var decoders = [];
         // shortcut to allow caller to default to "all non-label keys are data keys"
         var dataKeys = opts.dataKeys ? opts.dataKeys : _.keys(data[0]);
-        _.remove(dataKeys, labelKeys);
         // maybe a little too clever but also the simplest;
         // serialize every value for a given data key, then zip the results back up into a (possibly nested) array
         var transform = function (keys) {
@@ -72,7 +72,20 @@ var OneHotEncoder = /** @class */ (function () {
             }
         }
     };
+    /**
+   * Standardizing field
+   * Example dataset:
+   * [ { planet: 'mars', isGasGiant: false, value: 10 },
+   * { planet: 'saturn', isGasGiant: true, value: 20 },
+   * { planet: 'jupiter', isGasGiant: true, value: 30 } ]
+   *
+     * @param key: each key/feature such as planet, isGasGiant and value
+     * @param data: the entire dataset
+     * @param decoders: returned decoder
+     * @returns {any}
+     */
     OneHotEncoder.prototype.standardizeField = function (key, data, decoders) {
+        if (decoders === void 0) { decoders = []; }
         var type = typeof data[0][key];
         var values = _.map(data, key);
         // NOTE: this is calculating the sample standard deviation (vs population stddev).
@@ -85,6 +98,7 @@ var OneHotEncoder = /** @class */ (function () {
             case 'string': {
                 var oneHotEncoder = this.buildOneHot(values);
                 var encoded = _.map(values, oneHotEncoder.encode);
+                // TODO: Let's prefer immutable datastructure
                 decoders.push({
                     key: key,
                     type: type,
@@ -94,16 +108,19 @@ var OneHotEncoder = /** @class */ (function () {
                 return encoded;
             }
             case 'number': {
+                // Apply std to values if type is number
                 // standardize: ((n - mean)/std)
-                // todo add support for scaling to [0, 1]
+                // TODO: add support for scaling to [0, 1]
                 var mean_1 = _.mean(values);
                 var std_1 = calculateStd(values, mean_1);
                 decoders.push({ type: type, mean: mean_1, std: std_1, key: key });
                 return _.map(values, function (value) { return (value - mean_1) / std_1; });
             }
             case 'boolean': {
+                // True == 1
+                // False == 0
                 decoders.push({ type: type, key: key });
-                return _.map(values, function (value) { return (value ? 1 : 0); });
+                return this.booleanOneHot(values);
             }
             default:
                 return values;
@@ -121,13 +138,12 @@ var OneHotEncoder = /** @class */ (function () {
      * It's not ideal (ideally it would all just be done in-memory and we could return a "decode" closure,
      * but it needs to be serializable to plain old JSON.
      */
-    OneHotEncoder.prototype.buildOneHot = function (lst) {
+    OneHotEncoder.prototype.buildOneHot = function (list) {
         var lookup = {};
-        var lookupTable = [];
         var i = 0;
-        _.map(_.uniq(lst), function (value) {
+        var lookupTable = _.map(_.uniq(list), function (value) {
             lookup[value] = i++;
-            lookupTable.push(value);
+            return value;
         });
         return {
             lookupTable: lookupTable,
--- src/lib/preprocessing/data.ts
+++ src/lib/preprocessing/data.ts
@@ -5,7 +5,7 @@ export class OneHotEncoder {
    * encode
    *
    * @param data - list of records to encode
-   * @param opts - dataKeys: independent variables, labelKeys: dependent variables
+   * @param opts - dataKeys: independent variables, labelKeys: dependent variables; mandatory
    * @return {{data: Array, decoders: Array}} - see readme for full explanation
    */
   public encode(data, opts = { dataKeys: null, labelKeys: null }) {
@@ -14,12 +14,11 @@ export class OneHotEncoder {
 
     // shortcut to allow caller to default to "all non-label keys are data keys"
     const dataKeys = opts.dataKeys ? opts.dataKeys : _.keys(data[0]);
-    _.remove(dataKeys, labelKeys);
-
     // maybe a little too clever but also the simplest;
     // serialize every value for a given data key, then zip the results back up into a (possibly nested) array
     const transform = keys =>
       _.zip(..._.map(keys, key => this.standardizeField(key, data, decoders)));
+
     const features = transform(dataKeys);
     const labels = transform(labelKeys);
 
@@ -72,7 +71,19 @@ export class OneHotEncoder {
     }
   }
 
-  private standardizeField(key, data, decoders) {
+	/**
+   * Standardizing field
+   * Example dataset:
+   * [ { planet: 'mars', isGasGiant: false, value: 10 },
+   * { planet: 'saturn', isGasGiant: true, value: 20 },
+   * { planet: 'jupiter', isGasGiant: true, value: 30 } ]
+   *
+	 * @param key: each key/feature such as planet, isGasGiant and value
+	 * @param data: the entire dataset
+	 * @param decoders: returned decoder
+	 * @returns {any}
+	 */
+  private standardizeField(key, data, decoders = []) {
     const type = typeof data[0][key];
     const values = _.map(data, key);
 
@@ -85,8 +96,9 @@ export class OneHotEncoder {
 
     switch (type) {
       case 'string': {
-        const oneHotEncoder = this.buildOneHot(values);
+        const oneHotEncoder = this.buildStringOneHot(values);
         const encoded = _.map(values, oneHotEncoder.encode);
+        // TODO: Let's prefer immutable datastructure
         decoders.push({
           key,
           type,
@@ -97,8 +109,9 @@ export class OneHotEncoder {
       }
 
       case 'number': {
+        // Apply std to values if type is number
         // standardize: ((n - mean)/std)
-        // todo add support for scaling to [0, 1]
+        // TODO: add support for scaling to [0, 1]
         const mean = _.mean(values);
         const std = calculateStd(values, mean);
         decoders.push({ type, mean, std, key });
@@ -106,12 +119,27 @@ export class OneHotEncoder {
       }
 
       case 'boolean': {
+        // True == 1
+        // False == 0
         decoders.push({ type, key });
-        return _.map(values, value => (value ? 1 : 0));
+        const oneHotEncoder = this.buildBooleanOneHot();
+        return oneHotEncoder.encode(values);
       }
 
       default:
         return values;
+    }
+  }
+
+	/**
+   * Example usage:
+   * boolEncoder.encode(true) => 1
+   * boolEncoder.encode(false) => 0
+	 * @returns {{encode: ((values?) => any)}}
+	 */
+  private buildBooleanOneHot = () => {
+    return {
+      encode: values => _.map(values, value => (value ? 1 : 0))
     }
   }
 
@@ -127,14 +155,13 @@ export class OneHotEncoder {
    * It's not ideal (ideally it would all just be done in-memory and we could return a "decode" closure,
    * but it needs to be serializable to plain old JSON.
    */
-  private buildOneHot(lst) {
+  private buildStringOneHot(list) {
     const lookup = {};
-    const lookupTable = [];
     let i = 0;
 
-    _.map(_.uniq(lst), value => {
+    const lookupTable = _.map(_.uniq(list), value => {
       lookup[value] = i++;
-      lookupTable.push(value);
+      return value;
     });
 
     return {
