ad745228873d1bbd4e3d9683f18630109c6c2d3e
Jason Shin <visualbbasic@gmail.com>
2018-05-06 09:43:56 +1000

    working on metrix

---
+++ src/lib/matrics/index.ts
@@ -0,0 +1,19 @@
+import * as ConfusionMatrix  from 'ml-confusion-matrix';
+
+/**
+ * Reference: http://www.dataschool.io/simple-guide-to-confusion-matrix-terminology/
+ * Official API Doc: https://mljs.github.io/confusion-matrix/
+ * @param yTrue
+ * @param yPred
+ */
+export const confusion_matrix = ConfusionMatrix;
+
+// const trueLabels =      [0, 1, 0, 1, 1, 0];
+const trueLabels = [ [0, 1, 1], [1, 0, 0] ];
+const predictedLabels = [1, 1, 1, 1, 0, 0];
+
+const cm2 = new confusion_matrix([[13, 2], [10, 5]], ['cheese', 'dog']);
+
+console.log('acc', cm2.getAccuracy());
+console.log(cm2.getFalseCount());
+console.log(cm2.getIndex('dog'));
