32d821302e4aac279d669dd9fdb43e8e27413820
Jason Shin <visualbbasic@gmail.com>
2018-05-08 00:29:01 +1000

    wip: checkarray test

--- test/utils/validation.test.ts
+++ test/utils/validation.test.ts
@@ -1,3 +1,10 @@
-describe('checkArray', () => {
+import { checkArray } from "../../src/lib/utils/validation";
 
-})
+describe('checkArray', () => {
+  it('should check that it\'s not an array', () => {
+    const arr = null;
+    const result = checkArray(arr);
+    console.log('checking result', result);
+    expect(result.isArray).toBe(false);
+  })
+});
