33798d82bc97d963870f08dafd5ef1b8b6084d89
Jason Shin <visualbbasic@gmail.com>
2018-05-20 19:34:51 +1000

    adding tree

--- src/lib/svm/index.repl.ts
+++ src/lib/svm/index.repl.ts
@@ -1,5 +1,5 @@
 /* tslint:disable */
-import { SVC } from './classes';
+import { SVC, SVR } from './classes';
 
 console.log('running xor example');
 
@@ -8,7 +8,17 @@ async function xor() {
   const features = [[0, 0], [1, 1]];
   const labels = [0, 1];
   await svc.fit({ X: features, y: labels });
-  console.log('pred result', svc.predict([2., 2.]));
+  console.log('SVC predict result', svc.predict([2., 2.]));
 }
 
-xor().then(() => console.log('done!zzzz'));
+xor().then(() => console.log('finished SVC'));
+
+async function xor2() {
+  const svr = new SVR();
+  const features = [[0, 0], [2, 2]];
+  const labels = [0.5, 2.5];
+  await svr.fit({ X: features, y: labels });
+  console.log('SVR predict result', svr.predict([1, 1]));
+}
+
+xor2().then(() => console.log('finished SVR'));
