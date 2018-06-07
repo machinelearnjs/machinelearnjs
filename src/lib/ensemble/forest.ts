7e791ee77abb23064fb4a8626a0b24caea0a3173
jasons <jasons@metamako.com>
2018-06-07 21:29:42 +1000

    adding some docs for random forest

--- src/lib/ensemble/forest.ts
+++ src/lib/ensemble/forest.ts
@@ -4,14 +4,36 @@ import { DecisionTreeClassifier } from "../tree/tree";
 export class RandomForestClassifier {
 	private trees = [];
 	private y = null;
+	private nEstimator;
+	private criterion;
+	private randomState;
+
+	constructor(props = { nEstimator: 10, criterion: 'gini', randomState: null }) {
+		this.nEstimator = props.nEstimator;
+		this.criterion = props. criterion;
+		this.randomState = props.randomState;
+	}
+
+	/**
+	 * Fit data and build list of trees
+	 * @param {any} X
+	 * @param {any} y
+	 */
 	public fit({ X, y }) {
 		this.y = y;
-		for (let i = 0; i < 5; i++) {
+		this.trees = _.reduce(_.range(0, this.nEstimator), (sum) => {
 			const tree = new DecisionTreeClassifier({ featureLabels: null, randomise: true });
 			tree.fit({ X, y });
-			this.trees.push(tree);
-		}
+			return _.concat(sum, [tree]);
+		}, []);
+		console.log('checking tres', this.trees.length);
 	}
+
+	/**
+	 * Predict classification
+	 * @param X
+	 * @returns {string[]}
+	 */
 	public predict(X) {
 		let predictions = _.map(this.trees, (tree) => {
 			return tree.predict({ X });
@@ -19,6 +41,13 @@ export class RandomForestClassifier {
 		return this.baggingPrediction(predictions);
 	}
 
+	/**
+	 * Bagging prediction help method
+	 * According to the predictions returns by the trees, it will select the
+	 * class with the maximum number (votes)
+	 * @param {Array<any>} predictions
+	 * @returns {string[]}
+	 */
 	private baggingPrediction(predictions: Array<any>) {
 		const counts = _.countBy(predictions, x => x);
 		const countsArray = _.reduce(_.keys(counts), (sum, key) => {
