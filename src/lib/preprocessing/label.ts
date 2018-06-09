6d6355cb400a803330623f820ead3e84ba78a694
jasons <jasons@metamako.com>
2018-06-09 15:58:54 +1000

    Label encoder docs

--- src/lib/preprocessing/label.ts
+++ src/lib/preprocessing/label.ts
@@ -1,7 +1,15 @@
 import * as _ from 'lodash';
 
+/**
+ * Encode labels with value between 0 and n_classes-1.
+ */
 export class LabelEncoder {
   private classes: any[];
+
+	/**
+   * Fit label encoder
+	 * @param {any[]} X
+	 */
   public fit(X: any[] = null): void {
     if (_.isEmpty(X)) {
       throw new Error('X cannot be empty!');
@@ -10,9 +18,12 @@ export class LabelEncoder {
   }
 
   /**
-   * Transforms labels using fitted classes
+   * Transform labels to normalized encoding.
+   *
    * Given classes of ['amsterdam', 'paris', 'tokyo']
+   *
    * It transforms ["tokyo", "tokyo", "paris"]
+   *
    * Into [2, 2, 1]
    * @param X
    */
