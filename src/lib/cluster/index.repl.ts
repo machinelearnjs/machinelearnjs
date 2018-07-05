20dcf08dacffd69a25f0ad22bedaac514771ecdb
Jason Shin <yong.j.shin@student.uts.edu.au>
2018-07-05 23:37:43 +1000

    kmean_cluster 1. working on the distance calc step of "fit"

---
+++ src/lib/cluster/index.repl.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+import { KMeans } from "./k_means";
+
+const kmean = new KMeans();
+kmean.fit([[1, 2], [1, 4], [1, 0], [4, 2], [4, 4], [4, 0]]);
--- src/lib/cluster/k_means.ts
+++ src/lib/cluster/k_means.ts
@@ -1,42 +1,96 @@
 import * as _ from 'lodash';
-import * as kmeans from 'node-kmeans';
+import math from '../utils/MathExtra';
 
-interface KMeansOptions {
+export interface KMeansOptions {
 	/**
    * Number of clusters
 	 */
-	k: number,
+	k: number;
 	/**
    * Choice of distance method. Defaulting to euclidean
 	 */
-	distance: 'euclidean' | 'manhattan',
+	distance: 'euclidean' | 'manhattan';
+	/**
+   * Relative tolerance with regards to inertia to declare convergence
+	 */
+	maxIteration: number;
 }
 
 export class KMeans {
   private k: number;
-  private distance: string;
-  constructor(opions: KMeansOptions = {
-    k = 3,
-    distance = 'euclidean'
+  private distance;
+  private maxIteration: number;
+  private centroids: number[];
+
+  constructor(options: KMeansOptions = {
+    k: 3,
+    distance: 'euclidean',
+    maxIteration: 300
   }) {
-    this.k = _.get(opions, 'k', 3);
-    this.distance = _.get(options, 'distance', 'euclidean');
+    this.k = _.get(options, 'k', 3);
+    // Assigning a distance method
+    const distanceType = _.get(options, 'distance', 'euclidean');
+
+    switch (distanceType) {
+      case 'euclidean':
+        this.distance = math.contrib.euclideanDistance;
+        break;
+      case 'manhattan':
+        this.distance = math.contrib.manhattanDistance;
+        break;
+      default:
+        throw new Error(`Unknown distance type ${distanceType}`);
+    }
+
+    this.maxIteration = _.get(options, 'maxIteration', 300);
+    this.centroids = [];
   }
 
   private getInitialCentroids(X: number[], k: number) {
-    const centroidsRange = _.range(0, k);
-    return _.reduce(centroidsRange, (centroids, i) => {
-      return _.concat(centroids, X[i]);
-    }, []);
+    // Create an initial copy
+    let centroids = _.clone(X)
+    // Sort it randomly
+    // TODO: Random seed
+    centroids.sort(() => {
+      return (Math.round(Math.random()) - 0.5);
+    });
+    return centroids.slice(0, k);
+  }
+
+  private getClosestCentroids(data: number[], centroids: number[], distance) {
+    let min = Infinity;
+    let index = 0;
+    _.forEach(centroids, (centroid, i) => {
+      console.log('checking data', data, ' centroid ', centroid);
+      const dist = distance(data, centroid);
+      console.log('checking dist', dist);
+      if (dist < min) {
+        min = dist;
+        index = i;
+      }
+    });
+    return index;
   }
 
   public fit(X) {
+    this.centroids = this.getInitialCentroids(X, this.k);
+    console.warn(this.maxIteration);
+    // Looping only within the maxIteration boundary
+    for (let iter = 0; iter < 1; iter++) {
 
+      // find the distance between the point and cluster; choose the nearest centroid
+      const assignments = _.map(X, (data) => {
+        return this.getClosestCentroids(data, this.centroids, this.distance);
+      });
+      console.log('checking assignments', assignments);
+    }
   }
 }
 
 // Data source: LinkedIn
 /*
+import * as kmeans from 'node-kmeans';
+
 const data = [
   { company: 'Microsoft', size: 91259, revenue: 60420 },
   { company: 'IBM', size: 400000, revenue: 98787 },
