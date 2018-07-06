323bc30d93273697f5032ff2b3d90ac1aed7ab2a
Jason Shin <yong.j.shin@student.uts.edu.au>
2018-07-06 23:14:32 +1000

    kmeans_cluster: 1. example pred in index.repl.ts

--- src/lib/cluster/index.repl.ts
+++ src/lib/cluster/index.repl.ts
@@ -5,6 +5,5 @@ const kmean = new KMeans({ k: 2 });
 const clusters = kmean.fit({X: [[1, 2], [1, 4], [1, 0], [4, 2], [4, 4], [4, 0]]});
 console.log('checking clusters', clusters);
 
-kmean.predict({ X: [[0, 0], [4, 4]] });
-
-// Create the data 2D-array (vectors) describing the data
+const predResult = kmean.predict({ X: [[0, 0], [4, 4]] });
+console.log(predResult);
