ffd4cb72b9dde841f597c0f64c6ef59372365b6b
Jason Shin <yong.j.shin@student.uts.edu.au>
2018-07-08 12:20:19 +1000

    feature/test-svm: yarn fix

--- test/svm/classes.test.ts
+++ test/svm/classes.test.ts
@@ -1,76 +1,68 @@
 import * as _ from 'lodash';
-import {
-	NuSVC,
-	NuSVR,
-	OneClassSVM,
-	SVC,
-	SVR,
-} from '../../src/lib/svm/classes';
+import { NuSVC, NuSVR, OneClassSVM, SVC, SVR } from '../../src/lib/svm/classes';
 
-jest.mock('libsvm-js', () => () =>
-	({
-		predict: X => X,
-		predictOne: X => X,
-		train: (X, y) => ({X, y}),
-	})
-);
+jest.mock('libsvm-js', () => () => ({
+  predict: X => X,
+  predictOne: X => X,
+  train: (X, y) => ({ X, y })
+}));
 
 describe('svm:classes', () => {
-	const X1 = [[-1, -1], [-2, -1], [1, 1], [2, 1]];
-	const y1 = [-1, 1, 2, 2];
+  const X1 = [[-1, -1], [-2, -1], [1, 1], [2, 1]];
+  const y1 = [-1, 1, 2, 2];
 
-	it('should test SVC with X1 and y1, then return the same pred feed', () => {
-		const svc = new SVC();
-		return svc.fit({ X: X1, y: y1 }).then(() => {
-			const feed = [-0.8, -1];
-			const result = svc.predict(feed);
-			expect(_.isEqual(result, feed)).toBe(true);
-			const result2 = svc.predictOne(feed);
-			expect(_.isEqual(result2, feed)).toBe(true);
-		});
-	});
+  it('should test SVC with X1 and y1, then return the same pred feed', () => {
+    const svc = new SVC();
+    return svc.fit({ X: X1, y: y1 }).then(() => {
+      const feed = [-0.8, -1];
+      const result = svc.predict(feed);
+      expect(_.isEqual(result, feed)).toBe(true);
+      const result2 = svc.predictOne(feed);
+      expect(_.isEqual(result2, feed)).toBe(true);
+    });
+  });
 
-	it('should test SVR with X1 and y1, then return the same pred feed', () => {
-		const svr = new SVR();
-		return svr.fit({ X: X1, y: y1 }).then(() => {
-			const feed = [-0.8, -1];
-			const result = svr.predict(feed);
-			expect(_.isEqual(result, feed)).toBe(true);
-			const result2 = svr.predictOne(feed);
-			expect(_.isEqual(result2, feed)).toBe(true);
-		});
-	});
+  it('should test SVR with X1 and y1, then return the same pred feed', () => {
+    const svr = new SVR();
+    return svr.fit({ X: X1, y: y1 }).then(() => {
+      const feed = [-0.8, -1];
+      const result = svr.predict(feed);
+      expect(_.isEqual(result, feed)).toBe(true);
+      const result2 = svr.predictOne(feed);
+      expect(_.isEqual(result2, feed)).toBe(true);
+    });
+  });
 
-	it('should test OneClassSVM with X1 and y1, then return the same pred feed', () => {
-		const ocv = new OneClassSVM();
-		return ocv.fit({ X: X1, y: y1 }).then(() => {
-			const feed = [-0.8, -1];
-			const result = ocv.predict(feed);
-			expect(_.isEqual(result, feed)).toBe(true);
-			const result2 = ocv.predictOne(feed);
-			expect(_.isEqual(result2, feed)).toBe(true);
-		});
-	});
+  it('should test OneClassSVM with X1 and y1, then return the same pred feed', () => {
+    const ocv = new OneClassSVM();
+    return ocv.fit({ X: X1, y: y1 }).then(() => {
+      const feed = [-0.8, -1];
+      const result = ocv.predict(feed);
+      expect(_.isEqual(result, feed)).toBe(true);
+      const result2 = ocv.predictOne(feed);
+      expect(_.isEqual(result2, feed)).toBe(true);
+    });
+  });
 
-	it('should test NuSVC with X1 and y1, then return the same pred feed', () => {
-		const nusvc = new NuSVC();
-		return nusvc.fit({ X: X1, y: y1 }).then(() => {
-			const feed = [-0.8, -1];
-			const result = nusvc.predict(feed);
-			expect(_.isEqual(result, feed)).toBe(true);
-			const result2 = nusvc.predictOne(feed);
-			expect(_.isEqual(result2, feed)).toBe(true);
-		});
-	});
+  it('should test NuSVC with X1 and y1, then return the same pred feed', () => {
+    const nusvc = new NuSVC();
+    return nusvc.fit({ X: X1, y: y1 }).then(() => {
+      const feed = [-0.8, -1];
+      const result = nusvc.predict(feed);
+      expect(_.isEqual(result, feed)).toBe(true);
+      const result2 = nusvc.predictOne(feed);
+      expect(_.isEqual(result2, feed)).toBe(true);
+    });
+  });
 
-	it('should test NuSVR with X1 and y1, then return the same pred feed', () => {
-		const nusvr = new NuSVR();
-		return nusvr.fit({ X: X1, y: y1 }).then(() => {
-			const feed = [-0.8, -1];
-			const result = nusvr.predict(feed);
-			expect(_.isEqual(result, feed)).toBe(true);
-			const result2 = nusvr.predictOne(feed);
-			expect(_.isEqual(result2, feed)).toBe(true);
-		});
-	});
+  it('should test NuSVR with X1 and y1, then return the same pred feed', () => {
+    const nusvr = new NuSVR();
+    return nusvr.fit({ X: X1, y: y1 }).then(() => {
+      const feed = [-0.8, -1];
+      const result = nusvr.predict(feed);
+      expect(_.isEqual(result, feed)).toBe(true);
+      const result2 = nusvr.predictOne(feed);
+      expect(_.isEqual(result2, feed)).toBe(true);
+    });
+  });
 });
