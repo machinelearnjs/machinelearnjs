f717431906f3613ae1dbf6860033dde73d5c76e5
Jason Shin <yong.j.shin@student.uts.edu.au>
2018-07-11 20:46:15 +1000

    feature/test_accuracy_score 1. adding test for accuracyScore including testing exceptions and string inputs 2. lint fixes

--- test/metrics/classification.test.ts
+++ test/metrics/classification.test.ts
@@ -7,11 +7,14 @@ import {
 describe('classification:accuracy_score', () => {
   const yTrue1 = [0, 1, 2, 3];
   const yPred1 = [0, 2, 1, 3];
+  const yTrue2 = ['machine', 'learning', 'decision', 'tree'];
+  const yPred2 = ['learning', 'machine', 'decision', 'tree'];
+
   it('should yTrue1 and yPred1 return 0.5', () => {
     const expectedResult = 0.5;
     const result = accuracyScore({
+      y_pred: yPred1,
       y_true: yTrue1,
-      y_pred: yPred1
     });
 
     expect(result).toBe(expectedResult);
@@ -20,12 +23,61 @@ describe('classification:accuracy_score', () => {
   it('should yTrue1 and yPred1 with normalize:false return 2', () => {
     const expectedResult = 2;
     const result = accuracyScore({
-      y_true: yTrue1,
+      normalize: false,
       y_pred: yPred1,
-      normalize: false
+      y_true: yTrue1,
     });
 
     expect(result).toBe(expectedResult);
+  });
+
+  it('should yTrue2 and yPred2 return 0.5', () => {
+    const expectedResult = 0.5;
+    const result = accuracyScore({
+      y_pred: yPred2,
+      y_true: yTrue2,
+    });
+    expect(result).toBe(expectedResult);
+  });
+
+  it('should yTrue2 and yPred2 with normalize:false ', () => {
+    const expectedResult = 2;
+    const result = accuracyScore({
+      normalize: false,
+      y_pred: yPred2,
+      y_true: yTrue2,
+    });
+    expect(result).toBe(expectedResult);
+  });
+
+  it('should throw not equal in size exception', () => {
+    expect(() => {
+      accuracyScore({
+        normalize: false,
+        y_pred: yPred2,
+        y_true: [1],
+      });
+    }).toThrow('y_true and y_pred are not equal in size!');
+  });
+
+  it('should y_true non-array should throw an error', () => {
+    expect(() => {
+      accuracyScore({
+        normalize: false,
+        y_pred: yPred2,
+        y_true: true,
+      });
+    }).toThrow('y_true cannot be null or empty');
+  });
+
+  it('should y_pred non-array should throw an error', () => {
+    expect(() => {
+      accuracyScore({
+        normalize: false,
+        y_pred: 1,
+        y_true: yTrue2,
+      });
+    }).toThrow('y_pred cannot be null or empty');
   });
 });
 
