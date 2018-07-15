44e96f374bb86d87b65c7471a12856b95902684d
Jason Shin <yong.j.shin@student.uts.edu.au>
2018-07-15 20:10:49 +1000

    feature/doc-site-phase2 1. nested sidebar json

--- docs/processor/ConfigProcessor.ts
+++ docs/processor/ConfigProcessor.ts
@@ -16,6 +16,36 @@ export class ConfigProcessor extends BaseProcesser {
     }
   }
 
+	/**
+   * Build a sidebar JSON for nested navigations
+   * e.g. [{"title":"cluster","collapsable":false,"children":[["./cluster.KMeans","KMeans"]]}
+	 * @param apiChildren
+	 * @returns {Array}
+	 */
+  private buildSidebarJSON(apiChildren) {
+    return _.reduce(apiChildren, (sum, child) => {
+      const [module, name] = child.name.split('.');
+      const existingGroupIndex = _.findIndex(sum, (o) => o.title === module);
+      if (existingGroupIndex === -1) {
+        // If there's no existing module group according to the current child's name
+        // create a new definition and append it to the sum
+        const newDefinition = {
+          title: module,
+          collapsable: false,
+          children: [[`./${child.name}`, name]]
+        };
+        return _.concat(sum, [newDefinition]);
+      } else {
+        // If there's an existing module definition,
+        // then append the current child's definition to the children list
+        const existing = sum[existingGroupIndex];
+        const newChildren = _.concat(existing.children, [[`./${child.name}`, name]]);
+        const updated = _.set(existing, 'children', newChildren);
+        return _.set(sum, `[${existingGroupIndex}]`, updated);
+      }
+    }, []);
+  }
+
   public run({ apiChildren }) {
     if (_.isEmpty(apiChildren)) {
       throw Error('Cannot execute the processor because apiChildren is empty');
@@ -24,7 +54,7 @@ export class ConfigProcessor extends BaseProcesser {
 
     // 1. Build sidebar component list
     const extraConfig = {
-      apiSidebar: _.map(apiChildren, child => `./${child.name}`)
+      apiSidebar: this.buildSidebarJSON(apiChildren),
     };
     // 2. Writing extraConfig object as .vuepress/extra.json
     fs.writeFileSync(this.vuepressExtraConfigPath, JSON.stringify(extraConfig), 'utf-8');
