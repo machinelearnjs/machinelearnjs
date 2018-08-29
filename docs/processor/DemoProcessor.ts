f7bcca46edd2295bf6b8a906e3004a2208a327b1
Jason Shin <visualbbasic@gmail.com>
2018-08-29 21:19:41 +1000

    wip: playing around with the folder loop

--- docs/processor/DemoProcessor.ts
+++ docs/processor/DemoProcessor.ts
@@ -6,6 +6,16 @@ export class DemoProcessor extends BaseProcesser {
   private modulesPath = path.join(__dirname, '../../src/lib');
   public run(): void {
     const modules = fs.readdirSync(this.modulesPath);
-    console.log(modules);
+    // Retrieving the full module paths
+    const modulesPath = modules.map(mod => path.join(this.modulesPath, mod));
+    // Retrieving the full demo paths
+    const modulesDemoPath = modulesPath.map(mod => path.join(mod, '/demos'));
+    modulesDemoPath.forEach((mod) => {
+      if (fs.existsSync(mod)) {
+        const z = fs.readdirSync(mod);
+        console.log(mod, z);
+      }
+    });
+    console.log(modulesPath);
   }
 }
---
+++ docs/processor/play.ts
@@ -0,0 +1,4 @@
+import { DemoProcessor } from "./DemoProcessor";
+
+const dp = new DemoProcessor();
+dp.run();
