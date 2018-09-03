da342a0829a8ffee4145a1ba8ef186cc926cc2a3
Jason Shin <visualbbasic@gmail.com>
2018-09-03 19:40:31 +1000

    adding documentations for Boston dataset

--- src/lib/datasets/Boston.ts
+++ src/lib/datasets/Boston.ts
@@ -7,7 +7,33 @@
 import 'isomorphic-fetch';
 import { BaseDataset } from './BaseDataset';
 
+/**
+ * This dataset contains information collected by the U.S Census Service concerning housing in the area of Boston Mass.
+ * It was obtained from the StatLib archive (http://lib.stat.cmu.edu/datasets/boston),
+ * and has been used extensively throughout the literature to benchmark algorithms.
+ * However, these comparisons were primarily done outside of Delve and are thus somewhat suspect.
+ * The dataset is small in size with only 506 cases.
+ *
+ * The data was originally published by Harrison, D. and Rubinfeld, D.L.
+ * `Hedonic prices and the demand for clean air', J. Environ. Economics & Management, vol.5, 81-102, 1978.
+ *
+ * @example
+ * import { Boston } from "kalimdor/datasets";
+ *
+ * (async function() {
+ *   const bostonData = new Boston();
+ *   const {
+ *     data,
+ *     targets,
+ *     labels,
+ *   } = await bostonData.load();
+ * });
+ *
+ */
 export class Boston extends BaseDataset {
+  /**
+   * Load the dataset
+   */
   public async load(): Promise<{
     /**
      * Training data
