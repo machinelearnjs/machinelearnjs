8f5cf480bda24b9d90bb7f03b4f2cd3b772dfbac
Jason Shin <visualbbasic@gmail.com>
2018-10-04 16:48:27 +1000

    bug: fixing the import demo for kmeans cluster

--- src/lib/cluster/k_means.ts
+++ src/lib/cluster/k_means.ts
@@ -25,7 +25,7 @@ export interface KMeansOptions {
  * K-Means clustering
  *
  * @example
- * import { KMeans } from 'kalimdor/k_means';
+ * import { KMeans } from 'kalimdor/cluster';
  *
  * const kmean = new KMeans({ k: 2 });
  * const clusters = kmean.fit({ X: [[1, 2], [1, 4], [1, 0], [4, 2], [4, 4], [4, 0]] });
