c6a481c9c5dbb49ca47c4adc76aecbdefbf7847c
Christoph Reinbothe <c_k_r@gmx.de>
2018-10-04 18:06:37 +0200

    :checkered_flag: Make building and testing under Windows work
    Get rid of all bash related commands in package.json and replaced them with pure node solutions (e.g. del-cli and ncp instead of "rm -rf" and "cp")
    
    Replaced test-integration.sh with corresponding test-integration.js to enable cross-platform. Also built in check whether Docker is installed.

---
+++ scripts/test-integration.js
@@ -0,0 +1,44 @@
+const cp = require("child_process");
+
+
+function isDockerInstalled() {
+    let result = true
+    try {
+        cp.execSync("docker version")
+    } catch (err) {
+        result = false
+    }
+    return result
+}
+
+async function runIntegrationTests() {
+
+    if (isDockerInstalled()) {
+        const cmds = [
+            {
+                comment: "1. building a kalimdor docker image",
+                cmd: "docker build -t kalimdor:latest ."
+            },
+            {
+                comment: "2. Running build-prod.sh in a temporary container",
+                cmd: "docker run --rm -it kalimdor:latest './scripts/build-prod.sh'"
+            },
+        ]
+
+        cmds.forEach(cmd => {
+            console.log(cmd.comment)
+            cp.execSync(cmd.cmd)
+        })
+    } else {
+        console.warn("Docker does not seem to be properly installed. Skipping integration tests.")
+    }
+}
+
+runIntegrationTests()
+    .then(() => {
+        console.log("PASS Integration tests")
+    })
+    .catch(() => {
+        console.error("FAILED Integration tests")
+        process.exitCode = 1;
+    })
