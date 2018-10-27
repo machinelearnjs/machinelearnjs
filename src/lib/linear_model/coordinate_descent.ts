ba6f8f7afaf11c6e94a33fc9aab58e497926aad9
Jason Shin <visualbbasic@gmail.com>
2018-10-27 22:18:06 +1100

    chore: updating coordinate_descent.ts

--- src/lib/linear_model/coordinate_descent.ts
+++ src/lib/linear_model/coordinate_descent.ts
@@ -1,10 +1,3 @@
 /*
-import { normalize, PolynomialFeatures } from '../preprocessing';
-
-export class Lasso {
-  public fit(X): void {
-    const poly = new PolynomialFeatures();
-    const _X = normalize({ X: poly.transform({ X }) });
-  }
-}
+Reminder: implement Lasso, Ridge and ElasticNet here
 */
