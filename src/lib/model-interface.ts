117eb8c4f34ddef16e8ab2e7512961091b9f2d61
Ben <687794+benjaminmcdonald@users.noreply.github.com>
2018-10-27 12:12:06 +0900

    removed failing doc strings

--- src/lib/model-interface.ts
+++ src/lib/model-interface.ts
@@ -3,21 +3,16 @@ type Type2DMatrix = ReadonlyArray<ReadonlyArray<number>>
 export abstract class IFitPredict<T> {
   /**
    * Fit data to build model
-   * @param {number[][]} X - training values, inputs feature data
-   * @param {T} y - target values, matched outputs
    */
   public abstract fit(X: Type2DMatrix, y: ReadonlyArray<T>): void;
 
   /**
    * Predict multiple rows. Each row has a feature data for a prediction
-   * @param {number[][]} X - values to predict in Matrix format
-   * @returns {T[]}
    */
   public abstract predict(X: Type2DMatrix): ReadonlyArray<T>;
 
   /**
    * Restores model from a checkpoint
-   * @param json - model
    */
   public abstract fromJSON(json: any): void;
   /**
