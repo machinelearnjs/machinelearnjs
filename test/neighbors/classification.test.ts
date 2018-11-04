d41511c95129c2ffa9646c8b99699ef46426c089
Jason Shin <visualbbasic@gmail.com>
2018-11-05 01:30:59 +1100

    wip: refactoring KNN to use the new typing system

--- test/neighbors/classification.test.ts
+++ test/neighbors/classification.test.ts
@@ -1,5 +1,6 @@
 import * as _ from 'lodash';
-import { KNeighborsClassifier } from '../../src/lib/neighbors/classification';
+import { KNeighborsClassifier } from '../../src/lib/neighbors';
+import { matchExceptionWithSnapshot } from '../util_testing';
 
 describe('classification:KNeighborsClassifier', () => {
   const X1 = [[0, 0, 0], [0, 1, 1], [1, 1, 0], [2, 2, 2], [1, 2, 2], [2, 1, 2]];
@@ -9,7 +10,7 @@ describe('classification:KNeighborsClassifier', () => {
   const y2 = ['a', 'z'];
   it('should predict 1 for [1, 2] against the sample 1', () => {
     const knn = new KNeighborsClassifier();
-    knn.fit({ X: X1, y: y1 });
+    knn.fit(X1, y1);
     const expected = 1;
     const pred = knn.predict([1, 2]);
     expect(pred).toBe(expected);
@@ -17,7 +18,7 @@ describe('classification:KNeighborsClassifier', () => {
 
   it('should predict 0 for [0, 0, 0] against the sample 1', () => {
     const knn = new KNeighborsClassifier();
-    knn.fit({ X: X1, y: y1 });
+    knn.fit(X1, y1);
     const pred = knn.predict([0, 0, 0]);
     const expected = 0;
     expect(pred).toBe(expected);
@@ -25,7 +26,7 @@ describe('classification:KNeighborsClassifier', () => {
 
   it('should predict [ 1, 0, 1 ] for [ [1, 2], [0, 2], [9, 5] ]  against the sample 1', () => {
     const knn = new KNeighborsClassifier();
-    knn.fit({ X: X1, y: y1 });
+    knn.fit(X1, y1);
     const pred = knn.predict([[1, 2], [0, 2], [9, 5]]);
     const expected = [1, 0, 1];
     expect(_.isEqual(pred, expected)).toBe(true);
@@ -33,7 +34,7 @@ describe('classification:KNeighborsClassifier', () => {
 
   it('should predict [ 1, 0, 1 ] for [ [1, 2, 4], [0], [9, 5] ] against the sample 1', () => {
     const knn = new KNeighborsClassifier();
-    knn.fit({ X: X1, y: y1 });
+    knn.fit(X1, y1);
     const pred = knn.predict([[1, 2, 4], [0], [9, 5]]);
     const expected = [1, 0, 1];
     expect(_.isEqual(pred, expected)).toBe(true);
@@ -41,7 +42,7 @@ describe('classification:KNeighborsClassifier', () => {
 
   it('should reload the nearest neighbor and predict the same result', () => {
     const knn = new KNeighborsClassifier();
-    knn.fit({ X: X1, y: y1 });
+    knn.fit(X1, y1);
 
     // Before saving
     const pred = knn.predict([[1, 2, 4], [0], [9, 5]]);
@@ -59,7 +60,7 @@ describe('classification:KNeighborsClassifier', () => {
 
   it("should predict [ 'a', 'a', 'a' ] for [ [1, 2, 4], [0], [9, 5] ] against the sample 2", () => {
     const knn = new KNeighborsClassifier();
-    knn.fit({ X: X2, y: y2 });
+    knn.fit(X2, y2);
     const pred = knn.predict([[1, 2, 4], [0], [9, 5]]);
     const expected = ['a', 'a', 'a'];
     expect(_.isEqual(pred, expected)).toBe(true);
@@ -68,27 +69,19 @@ describe('classification:KNeighborsClassifier', () => {
   it('should throw an error if string is the first array element', () => {
     const expected = 'The dataset is neither an array or a matrix';
     const knn = new KNeighborsClassifier();
-    knn.fit({ X: X2, y: y2 });
+    knn.fit(X2, y2);
     expect(() => {
       knn.predict([[1, 'a', 4], [0], [9, 5]]);
     }).toThrow(expected);
   });
 
   it('should throw an error if X is not a matrix', () => {
-    const expected = 'X must be a matrix array!';
     const knn = new KNeighborsClassifier();
-    expect(() => {
-      // tslint:disable-next-line
-      knn.fit({ X: [1], y: [2] });
-    }).toThrow(expected);
+    matchExceptionWithSnapshot(knn.fit, [[1], [2]]);
   });
 
   it('should throw an error if y is not a vector', () => {
-    const expected = 'y must be a vector array!';
     const knn = new KNeighborsClassifier();
-    expect(() => {
-      // tslint:disable-next-line
-      knn.fit({ X: [[1], [2]], y: 123 });
-    }).toThrow(expected);
+    matchExceptionWithSnapshot(knn.fit, [[[1], [2]], 123]);
   });
 });
