28f65e5118d3e9c03e758db402e6a9e81ecc4dd7
Jason Shin <visualbbasic@gmail.com>
2018-11-18 14:41:53 +1100

    chore: install release-it 7.3.0 in the release-in-docker scripts

--- scripts/releases/docker_internal/release-in-docker.sh
+++ scripts/releases/docker_internal/release-in-docker.sh
@@ -77,4 +77,5 @@ echo "6. install packages"
 yarn
 
 echo "7. Run release-it patch with the given config"
+npm install -g release-it@7.3.0
 npx release-it ${VERSION} -n -c ./scripts/releases/configs/.release-it.json
