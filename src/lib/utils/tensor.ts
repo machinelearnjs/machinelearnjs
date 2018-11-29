1d3ced05778d04fe3c5cfe8dbf8222b69a24bc2d
Jason Shin <visualbbasic@gmail.com>
2018-11-29 20:21:51 +1100

    chore: adding more tests against the reshape method

--- src/lib/utils/tensor.ts
+++ src/lib/utils/tensor.ts
@@ -1,15 +1,33 @@
+import { flattenDeep } from 'lodash';
 import { TypeMatrix } from '../types/matrix.types';
 
 /**
- * Reshapes any size of array into a new shape
+ * Reshapes any size of array into a new shape. The code was copied from
+ * math.js, https://github.com/josdejong/mathjs/blob/5750a1845442946d236822505c607a522be23474/src/utils/array.js#L258
+ * in order to use specific method from Math.js instead of install an entire library.
  *
  * @example
  * reshape([1, 2, 3, 4, 5, 6], [2, 3]); // [[1, 2, 3], [4, 5, 6]]
  *
- * @param array
- * @param sizes
+ * @param array - Target array
+ * @param sizes - New array shape to resize into
+ * @ignore
  */
 export function reshape(array, sizes): TypeMatrix<any> {
+  // If the reshaping is to single dimensional
+  if (Array.isArray(array) && sizes.length === 1) {
+    const deepFlat = flattenDeep(array);
+    if (deepFlat.length === sizes[0]) {
+      return deepFlat;
+    } else {
+      throw new TypeError(
+        `Target array shape [${
+          deepFlat.length
+        }] cannot be reshaped into ${sizes}`
+      );
+    }
+  }
+
   // testing if there are enough elements for the requested shape
   let tmpArray = array;
   let tmpArray2;
