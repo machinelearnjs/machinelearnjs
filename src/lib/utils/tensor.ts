9b64d9f502762e92d49d6f6f1a14aa38386fb8db
Jason Shin <visualbbasic@gmail.com>
2018-11-29 20:06:49 +1100

    chore: adding reshape, copied from math.js

--- src/lib/utils/tensor.ts
+++ src/lib/utils/tensor.ts
@@ -1,17 +1,31 @@
-/* import * as tf from '@tensorflow/tfjs';
+import { TypeMatrix } from '../types/matrix.types';
 
-export function dataSyncRaw(
-  tensor: tf.Tensor1D | tf.Tensor2D | tf.Tensor3D | tf.Tensor4D
-): void {
-  const rawData = tensor.dataSync();
-  const shape = tensor.shape;
+/**
+ * Reshapes any size of array into a new shape
+ *
+ * @example
+ * reshape([1, 2, 3, 4, 5, 6], [2, 3]); // [[1, 2, 3], [4, 5, 6]]
+ *
+ * @param array
+ * @param sizes
+ */
+export function reshape(array, sizes): TypeMatrix<any> {
+  // testing if there are enough elements for the requested shape
+  let tmpArray = array;
+  let tmpArray2;
+  // for each dimensions starting by the last one and ignoring the first one
+  for (let sizeIndex = sizes.length - 1; sizeIndex > 0; sizeIndex--) {
+    const size = sizes[sizeIndex];
+    tmpArray2 = [];
 
-  // Recursion
-
-  // const buildMatrix
-
-  for (let i = 0; i < shape.length - 1; i++) {
-    const splitPoint = rawData.length / shape[i];
-    console.log(rawData, i, shape[i]);
+    // aggregate the elements of the current tmpArray in elements of the requested size
+    const length = tmpArray.length / size;
+    for (let i = 0; i < length; i++) {
+      tmpArray2.push(tmpArray.slice(i * size, (i + 1) * size));
+    }
+    // set it as the new tmpArray for the next loop turn or for return
+    tmpArray = tmpArray2;
   }
-} */
+
+  return tmpArray;
+}
--- test/utils/tensor.test.ts
+++ test/utils/tensor.test.ts
@@ -1,9 +1,10 @@
 import * as tf from '@tensorflow/tfjs';
-import { dataSyncRaw } from '../../src/lib/utils/tensor';
+import { reshape } from '../../src/lib/utils/tensor';
 
 describe('utils.dataSyncRaw', () => {
   it('tzz', () => {
     const x = tf.tensor2d([[1, 2, 3], [4, 5, 6]]);
-    dataSyncRaw(x);
+    const result = reshape([1, 2, 3, 4, 5, 6], [2, 3]);
+    console.info(result);
   });
 });
