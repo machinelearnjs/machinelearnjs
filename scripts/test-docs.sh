6d2c6641a24b4742ba13ed606d7801402ed51fd1
Jason Shin <visualbbasic@gmail.com>
2018-12-15 16:44:54 +1100

    chore: updating test-docs.sh

--- scripts/test-docs.sh
+++ scripts/test-docs.sh
@@ -4,7 +4,7 @@ echo "1. Building the project"
 yarn build
 
 echo "2. Copying test/docs/dummy.json to docs/docs.json"
-cp ./test/docs/dummy.json ./docs/docs.json
+cp ./test/docs/__snapshots__/docs.snapshot.json ./docs/docs.json
 
 echo "3. Running docs:helper target"
 npx jest -t "docs:helper"
