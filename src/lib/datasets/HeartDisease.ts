65c98381edd908ac9ebbc1c866e2947f1ac61786
Oleg Stotsky <ostotsky@gmail.com>
2019-03-25 18:41:33 +1000

    feature: add HeartDisease dataset

---
+++ src/lib/datasets/HeartDisease.ts
@@ -0,0 +1,17 @@
+import { BaseDataset } from './BaseDataset';
+
+export class HeartDisease extends BaseDataset {
+  public async load(): Promise<{
+    data: any[][];
+    targets: any[];
+    labels: string[];
+  }> {
+    const { data, targets, labels } = await this.fsLoad('heart_disease');
+
+    return {
+      data,
+      targets,
+      labels
+    };
+  }
+}
