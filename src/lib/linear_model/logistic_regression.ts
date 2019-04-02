a29ea1ed0ef08b1430b3192122141ea7be7b426d
Oleg Stotsky <ostotsky@gmail.com>
2019-04-02 18:02:54 +1000

    refactor: logistic regression

--- src/lib/linear_model/logistic_regression.ts
+++ src/lib/linear_model/logistic_regression.ts
@@ -9,18 +9,16 @@ import {
   tensor1d,
   tensor2d
 } from '@tensorflow/tfjs';
-import { map } from 'lodash';
-import { inferShape } from '../ops';
 import { Type1DMatrix, Type2DMatrix } from '../types';
+import { ensure2DMatrix, inferShape } from '../utils/tensors';
+import { checkNumFeatures } from '../utils/validation';
 
 export class LogisticRegression {
-  private params: Tensor1D;
+  private weights: Tensor1D;
   private learningRate: number;
-  private gradientDescent: boolean;
 
-  constructor(learningRate = 0.1, gradientDescent = true) {
+  constructor(learningRate = 0.1) {
     this.learningRate = learningRate;
-    this.gradientDescent = gradientDescent;
   }
 
   public fit(
@@ -28,61 +26,55 @@ export class LogisticRegression {
     y: Type1DMatrix<number>,
     numIterations = 4000
   ): void {
-    const xWrapped: Type2DMatrix<number> = this.makeX2DMatrix(X);
-    this.initParams(xWrapped);
+    const xWrapped: Type2DMatrix<number> = ensure2DMatrix(X);
+    this.initWeights(xWrapped);
     const tensorX = tensor2d(xWrapped);
     const tensorY = tensor1d(y);
 
     for (let i = 0; i < numIterations; ++i) {
-      const predictions: Tensor<Rank> = sigmoid(tensorX.dot(this.params));
+      const predictions: Tensor<Rank> = sigmoid(tensorX.dot(this.weights));
 
-      if (this.gradientDescent) {
-        const gradient: Tensor<Rank> = mul(
-          tensorY.sub(predictions).dot(tensorX),
-          -1
-        );
-        this.params = this.params.sub(mul(this.learningRate, gradient));
-      }
+      const gradient: Tensor<Rank> = mul(
+        tensorY.sub(predictions).dot(tensorX),
+        -1
+      );
+      this.weights = this.weights.sub(mul(this.learningRate, gradient));
     }
   }
 
   public predict(X: Type2DMatrix<number> | Type1DMatrix<number>): number[] {
-    this.checkShape(X);
+    checkNumFeatures(X, this.weights.arraySync());
 
-    const xWrapped: Type2DMatrix<number> = this.makeX2DMatrix(X);
+    const xWrapped: Type2DMatrix<number> = ensure2DMatrix(X);
 
-    return Array.from(
-      round(sigmoid(tensor2d(xWrapped).dot(this.params))).dataSync()
-    );
+    return round(sigmoid(tensor2d(xWrapped).dot(this.weights))).arraySync() as number[];
   }
 
-  private initParams(X: Type2DMatrix<number> | Type1DMatrix<number>): void {
+  public toJSON(): {
+    weights: number[];
+    learningRate: number;
+  } {
+    return {
+      weights: this.weights.arraySync(),
+      learningRate: this.learningRate
+    };
+  }
+
+  public fromJSON({
+    weights,
+    learningRate
+  }: {
+    weights: number[];
+    learningRate: number;
+  }): void {
+    this.weights = tensor1d(weights);
+    this.learningRate = learningRate;
+  }
+
+  private initWeights(X: Type2DMatrix<number> | Type1DMatrix<number>): void {
     const shape: number[] = inferShape(X);
     const numFeatures: number = shape[1];
     const limit: number = 1 / Math.sqrt(numFeatures);
-    this.params = randomUniform([numFeatures], -limit, limit);
-  }
-
-  private makeX2DMatrix(
-    X: Type2DMatrix<number> | Type1DMatrix<number>
-  ): Type2DMatrix<number> {
-    const shape: number[] = inferShape(X);
-    return shape.length === 2
-      ? (X as Type2DMatrix<number>)
-      : (map(X, x => [x]) as Type2DMatrix<number>);
-  }
-
-  private checkShape(X: Type2DMatrix<number> | Type1DMatrix<number>): void {
-    const xShape: number[] = inferShape(X);
-    const paramsShape: number[] = inferShape(
-      Array.from(this.params.dataSync())
-    );
-    const xNumFeatures = xShape.length === 1 ? 1 : xShape[1];
-    const modelNumFeatures = paramsShape[0];
-    if (xNumFeatures !== modelNumFeatures) {
-      throw new Error(
-        `Provided X has incorrect number of features. Should have: ${modelNumFeatures}, got: ${xNumFeatures}`
-      );
-    }
+    this.weights = randomUniform([numFeatures], -limit, limit);
   }
 }
