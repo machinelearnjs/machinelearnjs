6faade379ed9b7c26ef8f8cd89feb1dc899e7efa
Jason Shin <visualbbasic@gmail.com>
2019-04-03 22:54:43 +1100

    refactor(logistic_regression.ts): using * as tf instead of explicit imports

--- src/lib/linear_model/logistic_regression.ts
+++ src/lib/linear_model/logistic_regression.ts
@@ -1,19 +1,19 @@
-import { mul, randomUniform, Rank, round, sigmoid, Tensor, Tensor1D, tensor1d, tensor2d } from '@tensorflow/tfjs';
+import * as tf from '@tensorflow/tfjs';
 import { Type1DMatrix, Type2DMatrix } from '../types';
 import { ensure2DMatrix, inferShape } from '../utils/tensors';
 import { checkNumFeatures, validateFitInputs } from '../utils/validation';
 
 export class LogisticRegression {
-  private weights: Tensor1D;
+  private weights: tf.Tensor1D;
   private learningRate: number;
 
   constructor(
     {
-      learning_rate = 0.1,
+      learning_rate = 0.001,
     }: {
       learning_rate: number;
     } = {
-      learning_rate: 0.1,
+      learning_rate: 0.001,
     },
   ) {
     this.learningRate = learning_rate;
@@ -23,14 +23,14 @@ export class LogisticRegression {
     const xWrapped: Type2DMatrix<number> = ensure2DMatrix(X);
     validateFitInputs(xWrapped, y);
     this.initWeights(xWrapped);
-    const tensorX = tensor2d(xWrapped);
-    const tensorY = tensor1d(y);
+    const tensorX = tf.tensor2d(xWrapped);
+    const tensorY = tf.tensor1d(y);
 
     for (let i = 0; i < numIterations; ++i) {
-      const predictions: Tensor<Rank> = sigmoid(tensorX.dot(this.weights));
+      const predictions: tf.Tensor<tf.Rank> = tf.sigmoid(tensorX.dot(this.weights));
 
-      const gradient: Tensor<Rank> = mul(tensorY.sub(predictions).dot(tensorX), -1);
-      this.weights = this.weights.sub(mul(this.learningRate, gradient));
+      const gradient: tf.Tensor<tf.Rank> = tf.mul(tensorY.sub(predictions).dot(tensorX), -1);
+      this.weights = this.weights.sub(tf.mul(this.learningRate, gradient));
     }
   }
 
@@ -39,7 +39,7 @@ export class LogisticRegression {
 
     const xWrapped: Type2DMatrix<number> = ensure2DMatrix(X);
 
-    return round(sigmoid(tensor2d(xWrapped).dot(this.weights))).arraySync() as number[];
+    return tf.round(tf.sigmoid(tf.tensor2d(xWrapped).dot(this.weights))).arraySync() as number[];
   }
 
   public toJSON(): {
@@ -53,7 +53,7 @@ export class LogisticRegression {
   }
 
   public fromJSON({ weights, learningRate }: { weights: number[]; learningRate: number }): void {
-    this.weights = tensor1d(weights);
+    this.weights = tf.tensor1d(weights);
     this.learningRate = learningRate;
   }
 
@@ -61,6 +61,6 @@ export class LogisticRegression {
     const shape: number[] = inferShape(X);
     const numFeatures: number = shape[1];
     const limit: number = 1 / Math.sqrt(numFeatures);
-    this.weights = randomUniform([numFeatures], -limit, limit);
+    this.weights = tf.randomUniform([numFeatures], -limit, limit);
   }
 }
