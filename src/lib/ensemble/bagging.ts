ea17e400dda1dd8e93b9a57061778465fab732f9
Oleg Stotsky <ostotsky@gmail.com>
2019-05-27 21:26:37 +1000

    bagging: add default values for params; add new params to constructor

--- src/lib/ensemble/bagging.ts
+++ src/lib/ensemble/bagging.ts
@@ -1,5 +1,5 @@
 import { DecisionTreeClassifier } from '../tree';
-import { IMlModel, Type1DMatrix, Type2DMatrix } from '../types';
+import { Type1DMatrix, Type2DMatrix } from '../types';
 import { ValidationError } from '../utils/Errors';
 import math from '../utils/MathExtra';
 import { ensure2DMatrix, inferShape } from '../utils/tensors';
@@ -15,15 +15,19 @@ export class BaggingClassifier {
   private bootstrapFeatures: boolean;
   private estimators: any[] = [];
   private estimatorsFeatures: number[][] = [];
+  private maxSamplesIsFloat: boolean = true;
+  private maxFeaturesIsFloat: boolean = true;
 
   constructor({
     BaseEstimator = DecisionTreeClassifier,
     numEstimators = 10,
-    maxSamples,
-    maxFeatures,
-    bootstrapSamples,
-    bootstrapFeatures,
-    estimatorOptions,
+    maxSamples = 1.0,
+    maxFeatures = 1.0,
+    bootstrapSamples = false,
+    bootstrapFeatures = false,
+    estimatorOptions = {},
+    maxSamplesIsFloat = true,
+    maxFeaturesIsFloat = true,
   }: {
     BaseEstimator: any;
     numEstimators: number;
@@ -32,6 +36,8 @@ export class BaggingClassifier {
     bootstrapSamples: boolean;
     bootstrapFeatures: boolean;
     estimatorOptions: any;
+    maxSamplesIsFloat: boolean;
+    maxFeaturesIsFloat: boolean;
   }) {
     if (!Number.isInteger(maxSamples) && !this.maxSamplesIsInValidRange(maxSamples)) {
       throw new ValidationError('float maxSamples param must be in [0, 1]');
@@ -43,6 +49,8 @@ export class BaggingClassifier {
     this.maxFeatures = maxFeatures;
     this.bootstrapSamples = bootstrapSamples;
     this.bootstrapFeatures = bootstrapFeatures;
+    this.maxSamplesIsFloat = maxSamplesIsFloat;
+    this.maxFeaturesIsFloat = maxFeaturesIsFloat;
   }
 
   public fit(X: Type2DMatrix<number>, y: Type1DMatrix<number>): void {
@@ -60,6 +68,8 @@ export class BaggingClassifier {
         this.maxFeatures,
         this.bootstrapSamples,
         this.bootstrapFeatures,
+        this.maxSamplesIsFloat,
+        this.maxFeaturesIsFloat,
       );
       const sampleY = rowIndices.map((ind) => y[ind]);
       const estimator = new this.BaseEstimator(this.estimatorOptions);
