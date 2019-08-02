d5ddd6d19972d2a885bba31ff53b2beb9482c71b
Jason Shin <visualbbasic@gmail.com>
2019-08-02 17:51:43 +1000

    feature(webpack.config.js): adding webpack for browser bundling and an explicit index.ts for browser

---
+++ scripts/webpack/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/lib/index.web.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../../build/lib'),
+  },
+  node: {
+    fs: 'empty',
+  },
+};
