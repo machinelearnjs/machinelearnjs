8f176cef1a84b9276676c6426eae3a14bdbf56d7
JasonShin <visualbbasic@gmail.com>
2025-03-15 14:12:50 +1100

    fix: export LabelEncoder

--- src/lib/preprocessing/index.ts
+++ src/lib/preprocessing/index.ts
@@ -1,5 +1,14 @@
 import { add_dummy_feature, Binarizer, MinMaxScaler, normalize, OneHotEncoder, PolynomialFeatures } from './data';
-
 import { Imputer } from './Imputer';
+import { LabelEncoder } from './label';
 
-export { add_dummy_feature, Binarizer, MinMaxScaler, normalize, OneHotEncoder, PolynomialFeatures, Imputer };
+export {
+  add_dummy_feature,
+  Binarizer,
+  MinMaxScaler,
+  normalize,
+  OneHotEncoder,
+  PolynomialFeatures,
+  Imputer,
+  LabelEncoder,
+};
